528edab87c7f4ded3e22c9f4d47167ebd426aa45
simon-suuk <simon.suuk@gmail.com>
2017-09-18 15:07:15 +0000

    constructor added to initialize instance variables of contact class

--- ContactManager.py
+++ ContactManager.py
@@ -1,2 +1,7 @@
 class Contact:
-    pass
+    def __init__(self, name=None, phone_no="Cylindrical", email=None, gender=None, post_address=None):
+        self.name = name
+        self.phone_no = phone_no
+        self.email = email
+        self.gender = gender
+        self.post_address = post_address
