75892e8c6b541bd0d1895a72dfc44fc90a26d236
simon-suuk <simon.suuk@gmail.com>
2017-09-22 01:37:10 +0000

    added feature to store contacts in a file. added feature to search contact by name from a file. removing contact from a file in progress

--- ContactManager.py
+++ ContactManager.py
@@ -11,8 +11,18 @@ def capture_input():
                 gender=g, post_address=p_adr)
 
 
+def show_contact_details(name, phone_no, email, gender, post_address):
+    print("\n***--------------***----------------***")
+    print("Name: {}"
+          "\nPhone: {}"
+          "\nEmail: {}"
+          "\nGender: {}"
+          "\nAddress: {}".format(name, phone_no, email, gender, post_address))
+    print("***--------------***----------------***")
+
+
 class Contact:
-    def __init__(self, name=None, phone_no="Cylindrical",
+    def __init__(self, name=None, phone_no=0,
                  email=None, gender=None, post_address=None):
         self.name = name
         self.phone_no = phone_no
@@ -23,6 +33,21 @@ class Contact:
     def __repr__(self):
         return self.__dict__
 
+    def get_name(self):
+        return self.name
+
+    def get_phone_no(self):
+        return self.phone_no
+
+    def get_email(self):
+        return self.email
+
+    def get_gender(self):
+        return self.gender
+
+    def get_post_address(self):
+        return self.post_address
+
     def update_contact_details(self, **kwargs):
         self.name = kwargs.get("name")
         self.phone_no = kwargs.get("phone_no")
@@ -30,41 +55,35 @@ class Contact:
         self.gender = kwargs.get("gender")
         self.post_address = kwargs.get("post_address")
 
-    def show_contact_details(self):
-        print("\n***--------------***----------------***")
-        print("Name: {name}"
-              "\nPhone: {phone_no}"
-              "\nEmail: {email}"
-              "\nGender: {gender}"
-              "\nAddress: {post_address}".format(**self.__repr__()))
-        print("***--------------***----------------***")
-
 
 class ContactManager:
-    def __init__(self, contacts_file=None):
-        if contacts_file is None:
-            with open("temp_file.csv", "w") as newFile:
-                self.contacts_file = csv.writer(newFile)
-                self.contacts_file.writerow("")
+    def __init__(self, contacts_file=""):
+        if contacts_file == "":
+            self.contacts_file = "temp_file.csv"
         else:
-            with open(contacts_file, "w") as newFile:
-                self.contacts_file = csv.writer(newFile)
-                self.contacts_file.writerow("")
+            self.contacts_file = contacts_file
 
     def add_contact(self, contact):
-        self.contacts.append(contact)
+        with open(self.contacts_file, 'a', newline="") as file_to_append_to:
+            add_contact_file_writer = csv.writer(file_to_append_to)
+            add_contact_file_writer.writerow([contact.get_name(), contact.get_phone_no(),
+                                             contact.get_email(), contact.get_gender(),
+                                             contact.get_post_address()])
 
     def remove_contact(self, name):
-        for contact in self.contacts:
-            if contact.name == name:
-                self.contacts.remove(contact)
-                return contact
+        with open(self.contacts_file, 'r', newline="") as file_to_search:
+            search_contact_file_reader = csv.reader(file_to_search)
+            for row in search_contact_file_reader:
+                if name in row:
+                    return row[0], row[1], row[2], row[3], row[4]
         return None
 
     def search_contact(self, name):
-        for contact in self.contacts:
-            if contact.name == name:
-                return contact
+        with open(self.contacts_file, 'r', newline="") as file_to_search:
+            search_contact_file_reader = csv.reader(file_to_search)
+            for row in search_contact_file_reader:
+                if name in row:
+                    return row[0], row[1], row[2], row[3], row[4]
         return None
 
 
@@ -92,12 +111,13 @@ if __name__ == "__main__":
     # print contact details
     # new_contact.show_contact_details()
 
-    contact_list = ContactManager()
+    contact_list = ContactManager("contact_list.csv")
 
     contact_list.add_contact(new_contact)
 
     search_results = contact_list.search_contact("simon")
     if search_results is not None:
-        search_results.show_contact_details()
+        r_name, r_phone_no, r_email, r_gender, r_post_address = search_results
+        show_contact_details(r_name, r_phone_no, r_email, r_gender, r_post_address)
     else:
         print("search result: {}".format(search_results))
