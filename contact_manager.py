dd21e2d78d5496e6fc473f612646b1d5be46d893
simon-suuk <simon.suuk@gmail.com>
2017-09-25 14:20:15 +0000

    added mutators. added interactive loop to keep program running until user explicitly exits

--- contact_manager.py
+++ contact_manager.py
@@ -49,6 +49,21 @@ class Contact:
     def get_post_address(self):
         return self.post_address
 
+    def set_name(self, name):
+        self.name = name
+
+    def set_phone_no(self, phone_no):
+        self.phone_no = phone_no
+
+    def set_email(self, email):
+        self.email = email
+
+    def set_gender(self, gender):
+        self.gender = gender
+
+    def set_post_address(self, post_address):
+        self.post_address = post_address
+
     def update_contact_details(self, **kwargs):
         self.name = kwargs.get("name")
         self.phone_no = kwargs.get("phone_no")
@@ -99,41 +114,53 @@ if __name__ == "__main__":
           "\nYou can use the following commands:"
           "\n 1. \"a\" to add new contact"
           "\n 2. \"s\" to search contact"
-          "\n 3. \"r\" to remove contact\n")
+          "\n 3. \"r\" to remove contact"
+          "\n 4. \"e\" to edit contact\n")
 
     # get command issued by user
-    command = input()
-
-    # get user inputs returned as a dictionary
-    # unpack the values to respective instance variables
-    c_name, c_phone_no, c_email, c_gender, c_post_address = capture_input().values()
-
-    # create new contact object with user inputs
-    new_contact = Contact(c_name, c_phone_no, c_email, c_gender, c_post_address)
-
-    # perform update to modify contact details
-    # update_details = capture_input()
-    # new_contact.update_contact_details(**update_details)
-
-    # print contact details
-    # new_contact.show_contact_details()
+    command = input("command? ---> ")
 
     contact_list = ContactManager("contact_list.csv")
 
-    contact_list.add_contact(new_contact)
+    while command != "q":
+        if command == "a":
+            # get user inputs returned as a dictionary
+            # unpack the values to respective instance variables
+            c_name, c_phone_no, c_email, c_gender, c_post_address = capture_input().values()
 
-    # search for contact by name
-    search_results = contact_list.search_contact("elijah")
-    if search_results is not None:
-        r_name, r_phone_no, r_email, r_gender, r_post_address = search_results
-        show_contact_details(r_name, r_phone_no, r_email, r_gender, r_post_address)
-    else:
-        print("search result: {}".format(search_results))
+            # create new contact object with user inputs
+            new_contact = Contact(c_name, c_phone_no, c_email, c_gender, c_post_address)
 
-    # delete a contact by name
-    remove_results = contact_list.remove_contact("baaman")
-    if remove_results is not None:
-        r_name, r_phone_no, r_email, r_gender, r_post_address = remove_results
-        show_contact_details(r_name, r_phone_no, r_email, r_gender, r_post_address)
-    else:
-        print("remove result: {}".format(search_results))
+            # add contact to list
+            contact_list.add_contact(new_contact)
+        elif command == "s":
+            contact_name = input("contact name? ---> ")
+            # search for contact by name
+            search_results = contact_list.search_contact(contact_name)
+            if search_results is not None:
+                r_name, r_phone_no, r_email, r_gender, r_post_address = search_results
+                show_contact_details(r_name, r_phone_no, r_email, r_gender, r_post_address)
+            else:
+                print("search result: {}".format(search_results))
+                print("")
+        elif command == "r":
+            contact_name = input("contact name? ---> ")
+            # delete a contact by name
+            remove_results = contact_list.remove_contact(contact_name)
+            if remove_results is not None:
+                r_name, r_phone_no, r_email, r_gender, r_post_address = remove_results
+                show_contact_details(r_name, r_phone_no, r_email, r_gender, r_post_address)
+            else:
+                print("remove result: {}".format(remove_results))
+                print("")
+        elif command == "e":
+            pass
+            # perform update to modify contact details
+            # update_details = capture_input()
+            # new_contact.update_contact_details(**update_details)
+        else:
+            print("Sorry, I don't understand you")
+
+        # get command issued by user
+        command = input("command? ---> ")
+        print("")
